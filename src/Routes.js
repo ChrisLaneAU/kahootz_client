120f3b133a21918c555cdee54dc918e4e1c8535c
ChrisLaneAU <chrislane10001@gmail.com>
2019-05-11 15:16:40 +1000

    enabled passing props to dynamic urls with WaitingRoom to AskQuestions

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import {HashRouter as Router, Route} from 'react-router-dom'
-import JoinGame from './components/JoinGame/JoinGame'
-
-
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import JoinGame from "./components/JoinGame/JoinGame";
+import WaitingRoom from "./components/WaitingRoom/WaitingRoom";
+import AskQuestions from "./components/AskQuestions/AskQuestions";
 
 const Routes = (
-    <Router>
-        <div>
-            <Route exact path="/" component={JoinGame} />
-        </div>
-    </Router>
-)
+  <Router>
+    <div>
+      <Route exact path="/" component={JoinGame} />
+      <Route exact path="/waiting-room" component={WaitingRoom} />
+      <Route
+        exact
+        path="/game/:id"
+        render={props => <AskQuestions {...props} />}
+      />
+    </div>
+  </Router>
+);
 
 export default Routes;
---
+++ src/components/WaitingRoom/WaitingRoom.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+class WaitingRoom extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      question_id: "",
+      question: "",
+      answers: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:3000/quizes.json").then(quizzes => {
+      const question = quizzes.data[0].questions[0];
+      console.log("question", question);
+      console.log("data", quizzes.data[0]);
+      this.setState({
+        question_id: question.id,
+        question: question.content,
+        answers: question.answers
+      });
+    });
+  }
+
+  render() {
+    const { question_id, question, answers } = this.state;
+    console.log(question_id);
+    return (
+      <>
+        <h1>Waiting Room</h1>
+        <Link
+          to={{
+            pathname: `/game/${question_id}`,
+            state: {
+              question_id: question_id,
+              question: question,
+              answers: answers
+            }
+          }}
+        >
+          Start Game
+        </Link>
+        <p>{JSON.stringify(this.state.quiz)}</p>
+      </>
+    );
+  }
+}
+
+export default WaitingRoom;
