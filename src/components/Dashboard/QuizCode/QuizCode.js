ae72c9e5bd1a480b6bf89daecf843c296e401a0e
Ben Muller <ben.k.muller@gmail.com>
2019-05-12 18:07:31 +1000

    QuizCode added

---
+++ src/components/Dashboard/QuizCode/QuizCode.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+
+const SERVER_URL_PUT = 'http://localhost:3000/new_game.json' 
+const SERVER_URL_GET = 'http://localhost:3000/new_game.json'
+
+const QuizCode = () => {
+
+  let [ gameNum, getGameNum ] = useState(0)
+
+  console.log(gameNum);
+
+  useEffect(() => {
+     axios.post(SERVER_URL_PUT, { new_game: true } ).then((results) => {
+       axios.get(SERVER_URL_GET).then((result) => {
+        console.log('This is the GET result ', result);
+
+        getGameNum(result.data.id)
+       })
+     })
+
+  }, [])
+
+  return(
+    <h1>QuizCode: { gameNum * 9999 } </h1> //* 9999 just makes a number 6 digits long
+  );
+}
+
+export default QuizCode;
