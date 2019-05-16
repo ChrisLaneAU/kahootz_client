a094e01cd39374722f08d30a4f4c3c1a0f9c0610
ChrisLaneAU <chrislane10001@gmail.com>
2019-05-17 01:00:22 +1000

    fixed bug that printed html entities as strings in Question

--- src/components/PlayGame/Question/Question.js
+++ src/components/PlayGame/Question/Question.js
@@ -1,11 +1,33 @@
 import React from "react";
-import "./Question.scss"
+import "./Question.scss";
+
+const decodeEntities = (function() {
+  // this prevents any overhead from creating the object each time
+  var element = document.createElement("div");
+
+  function decodeHTMLEntities(str) {
+    if (str && typeof str === "string") {
+      // strip script/html tags
+      str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gim, "");
+      str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gim, "");
+      element.innerHTML = str;
+      str = element.textContent;
+      element.textContent = "";
+    }
+
+    return str;
+  }
+
+  return decodeHTMLEntities;
+})();
+
 const Question = ({ question }) => {
-  return(
-    <div className = "question">
-    <p>{question}</p>
+  const questionDecoded = decodeEntities(question);
+  return (
+    <div className="question">
+      <p>{questionDecoded}</p>
     </div>
-  ) 
+  );
 };
 
 export default Question;
