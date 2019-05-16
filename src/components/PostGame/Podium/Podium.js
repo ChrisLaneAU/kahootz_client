2b4dca70338c1008abe608c23087d007be734dcf
ChrisLaneAU <chrislane10001@gmail.com>
2019-05-17 00:44:23 +1000

    setup podium

--- src/components/PostGame/Podium/Podium.js
+++ src/components/PostGame/Podium/Podium.js
@@ -1,45 +1,63 @@
-import React, { Component } from 'react';
-import "./Podium.scss"
-
+import React, { Component } from "react";
+import { gamesRef } from "../../../config/firebase";
+import "./Podium.scss";
 
 class Podium extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      players: []
+    };
+  }
 
+  componentDidMount() {
+    gamesRef.child(`${this.props.gamePin}/players`).once("value", snapshot => {
+      this.setState({
+        players: Object.values(snapshot.val())
+          .sort((a, b) => a.score - b.score)
+          .reverse()
+      });
+    });
+  }
 
-    render() {
-
-        return (
-            <div className="display"> 
-                <div className="podium"> 
-                    <div className="podium__second"> 
-                        <div className="podium__second--player">
-                            Chris
-                        </div>
-                        <div className="podium__second--place">
-                            <p>2nd</p>
-                        </div>
-                    </div>
-                    <div className="podium__first">
-                        <div className="podium__first--player">
-                            Ben
-                        </div>
-                        <div className="podium__first--place">
-                            <p>1st</p>
-                        </div>
-                    </div>
-                    <div className="podium__third">
-                    <div className="podium__third--player">
-                            Jimmy
-                        </div>
-                        <div className="podium__third--place">
-                            <p>3rd</p>
-                        </div>
-                    </div>
-                </div>
+  renderPodium() {
+    const { players } = this.state;
+    return (
+      <div className="display">
+        <div className="podium">
+          <div className="podium__second">
+            <div className="podium__second--player">
+              {players[1].nickname} ({players[1].score} points)
             </div>
+            <div className="podium__second--place">
+              <p>2nd</p>
+            </div>
+          </div>
+          <div className="podium__first">
+            <div className="podium__first--player">
+              {players[0].nickname} ({players[0].score} points)
+            </div>
+            <div className="podium__first--place">
+              <p>1st</p>
+            </div>
+          </div>
+          <div className="podium__third">
+            <div className="podium__third--player">
+              {players[2].nickname} ({players[2].score} points)
+            </div>
+            <div className="podium__third--place">
+              <p>3rd</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-        )
-    }
+  render() {
+    return this.state.players.length > 0 ? this.renderPodium() : <></>;
+  }
 }
 
 export default Podium;
--- src/components/PostGame/PostGame.js
+++ src/components/PostGame/PostGame.js
@@ -1,39 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./PostGame.scss"
-import Podium from "./Podium/Podium"
-import { gamesRef } from '../../config/firebase'
-import _ from 'underscore'
-
+import "./PostGame.scss";
+import Podium from "./Podium/Podium";
+import { gamesRef } from "../../config/firebase";
+import _ from "underscore";
 
 const PostGame = props => {
-  const gamePin = props.location.state.pin
-  const gameRef = gamesRef.child(gamePin)
-  let first = ''
-  let second = ''
-  let third = ''
+  const gamePin = props.location.state.pin;
+  const gameRef = gamesRef.child(gamePin);
+  let first = "";
+  let second = "";
+  let third = "";
 
-  gameRef.once('value', snapshot => {
-   let players = snapshot.val().players
-   players = Object.values(players)
-   let playersSorted = _.sortBy( players, 'score') /// DOESNT WORK FIX
-   console.log(playersSorted);
-   first = playersSorted[0]
-   second = playersSorted[1]
-   third = playersSorted[2]
-   
-
-  })
+  gameRef.once("value", snapshot => {
+    let players = snapshot.val().players;
+    players = Object.values(players);
+    let playersSorted = _.sortBy(players, "score"); /// DOESNT WORK FIX
+    //console.log(playersSorted);
+    first = playersSorted[0];
+    second = playersSorted[1];
+    third = playersSorted[2];
+  });
 
   return (
     <>
       <div className="podium__header">
-          <h1>And The Winner Is...</h1>
-          <Link className="playagain_button" to="/">Play Another Game</Link>
-        </div>
-        
-      <Podium />
-      
+        <h1>And The Winner Is...</h1>
+        <Link className="playagain_button" to="/">
+          Play Another Game
+        </Link>
+      </div>
+
+      <Podium gamePin={gamePin} />
     </>
   );
 };
