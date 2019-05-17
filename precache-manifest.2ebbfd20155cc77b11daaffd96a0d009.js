27473d0b016a18136f23219ad9b21bdae84a5ea6
ChrisLaneAU <chrislane10001@gmail.com>
2019-05-17 11:51:04 +1000

    Updates

---
+++ precache-manifest.2ebbfd20155cc77b11daaffd96a0d009.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a5b96bf0b646472e612632eb7b9087",
+    "url": "/kahootz_client/index.html"
+  },
+  {
+    "revision": "36218c5955cc7fd13c5e",
+    "url": "/kahootz_client/static/css/main.2e266610.chunk.css"
+  },
+  {
+    "revision": "8f92ac87c0529cce7ed1",
+    "url": "/kahootz_client/static/js/2.1907d9a0.chunk.js"
+  },
+  {
+    "revision": "36218c5955cc7fd13c5e",
+    "url": "/kahootz_client/static/js/main.b5b74204.chunk.js"
+  },
+  {
+    "revision": "3ffc00ff15f4a6f677fc",
+    "url": "/kahootz_client/static/js/runtime~main.794ba248.js"
+  },
+  {
+    "revision": "074e55f49d05b03c94060bb4609a4295",
+    "url": "/kahootz_client/static/media/boardgame.074e55f4.jpg"
+  },
+  {
+    "revision": "9789951c5bc4e590d53d43bb54e30225",
+    "url": "/kahootz_client/static/media/books.9789951c.jpg"
+  },
+  {
+    "revision": "825a3a574a3e9f3ada9d05660aef4be2",
+    "url": "/kahootz_client/static/media/cartoon.825a3a57.jpg"
+  },
+  {
+    "revision": "9c6d41d802408a4f87013bc0e90b3d49",
+    "url": "/kahootz_client/static/media/celebrity.9c6d41d8.jpg"
+  },
+  {
+    "revision": "e8ae6f9b36a5254c67e38bf7726eb007",
+    "url": "/kahootz_client/static/media/computer.e8ae6f9b.jpg"
+  },
+  {
+    "revision": "825fb07779bad95624b9b8831635db20",
+    "url": "/kahootz_client/static/media/film.825fb077.jpg"
+  },
+  {
+    "revision": "e933a49a0b5b9d9795535b2657f545a9",
+    "url": "/kahootz_client/static/media/generalknowledge.e933a49a.jpg"
+  },
+  {
+    "revision": "7da4fc34010c90866b98d89080130388",
+    "url": "/kahootz_client/static/media/geography.7da4fc34.jpg"
+  },
+  {
+    "revision": "3e37782de594947c5909f02b3e2387e7",
+    "url": "/kahootz_client/static/media/history.3e37782d.jpg"
+  },
+  {
+    "revision": "6fe4f5b490f179b10225a366e60c54b4",
+    "url": "/kahootz_client/static/media/manga.6fe4f5b4.jpg"
+  },
+  {
+    "revision": "308f1b9fed495869f52f956b54e536e7",
+    "url": "/kahootz_client/static/media/music.308f1b9f.jpg"
+  },
+  {
+    "revision": "65c5f1ec6440cc1ac692a3946546eccf",
+    "url": "/kahootz_client/static/media/myth.65c5f1ec.jpg"
+  },
+  {
+    "revision": "38cd0a3b3407304acb54248b1b638ce2",
+    "url": "/kahootz_client/static/media/nature.38cd0a3b.jpg"
+  },
+  {
+    "revision": "4ec07cf34cb8abbbdb95eb91e6d65845",
+    "url": "/kahootz_client/static/media/sport.4ec07cf3.jpg"
+  },
+  {
+    "revision": "110c2fd84f9d50954e9c817de4275585",
+    "url": "/kahootz_client/static/media/television.110c2fd8.jpg"
+  },
+  {
+    "revision": "973793ae0fb8833a9b0774da1aa87d37",
+    "url": "/kahootz_client/static/media/vehicle.973793ae.jpg"
+  },
+  {
+    "revision": "181ef81bedccd5f2dab11dbde0f2a6c2",
+    "url": "/kahootz_client/static/media/videogame.181ef81b.jpg"
+  }
+]);
